26acf14147f39149b2d2a98f27d38d505af0a90d
lcy0x1 <arthurlcy0x1@gmail.com>
2019-11-06 23:03:30 -0800

    add local storage

--- src/store.js
+++ src/store.js
@@ -5,57 +5,84 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    quarter: '',
-    selected: [],
+    quarter: (() => { const a = localStorage.quarter; return a ? a : ""; })(),
+    selected: (() => { const a = localStorage.selected; return a ? JSON.parse(a) : []; })(),
     course: null,
-    events: [],
-    limit: {
-      timerange: [new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 23, 59)],
-      break: 10,
-    },
+    events: (() => { const a = localStorage.events; return a ? JSON.parse(a) : []; })(),
+    limit: (() => {
+      const a = localStorage.limit;
+      return a ? JSON.parse(a) : {
+        timerange: [new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 23, 59)],
+        break: 10,
+      }
+    })(),
     results: [],
   },
   mutations: {
     setQuarter(state, quarter) {
+      if (state.quarter != quarter) {
+        state.selected = [];
+        localStorage.selected = JSON.stringify(state.selected);
+      }
       state.quarter = quarter;
+
+      localStorage.quarter = quarter;
     },
+
     addSelected(state, course) {
       for (let i in state.selected) {
         if (state.selected[i] === course) return;
       }
       state.selected.push(course);
+
+      localStorage.selected = JSON.stringify(state.selected);
     },
+
     removeSelected(state, course) {
       for (let i in state.selected) {
         if (state.selected[i] === course) {
           state.selected.splice(i, 1);
         }
       }
+
+      localStorage.selected = JSON.stringify(state.selected);
     },
+
     setCourse(state, course) {
       state.course = course;
     },
+
     addEvents(state, event) {
       for (let i in state.events) {
         if (state.events[i].name === event.name) return;
       }
       state.events.push(event);
+
+      localStorage.events = JSON.stringify(state.events);
     },
+
     removeEvents(state, event) {
       for (let i in state.events) {
         if (state.events[i].name === event.name) {
           state.events.splice(i, 1);
         }
       }
+
+      localStorage.events = JSON.stringify(state.events);
     },
+
     setLimit(state, limit) {
       state.limit = limit;
+
+      localStorage.limit = JSON.stringify(state.limit);
     },
+
     addResults(state, result) {
       let copy = [];
       for (let i of result) copy.push(i);
       state.results.push(copy);
     },
+
     clearResults(state) {
       state.results = [];
     }
